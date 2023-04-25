3da7cb14d0cc38bd333cb69b911a1632324d3c07
numdinkushi <numdinkushi@gmail.com>
2023-04-25 15:54:36 +0100

    wip: products

---
+++ database/migrations/2023_04_25_103815_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->mediumText('summary');
+            $table->mediumText('description');
+            $table->integer('stock')->default(0);
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('child_category_id')->nullable();
+            $table->string('photo');
+            $table->float('price')->default(0);
+            $table->float('offer_price')->default(0);
+            $table->float('discount')->default(0);
+            $table->string('size');
+            $table->enum('conditions', ['new', 'popular', 'winter'])->default('new');
+            $table->unsignedBigInteger('vendor_id')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('child_category_id')->references('id')->on('categories')->onDelete('SET NULL');
+            $table->foreign('vendor_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
