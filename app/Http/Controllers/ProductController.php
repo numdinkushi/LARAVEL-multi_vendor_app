27fd6dbdbc6d92d67eb8e2cfedb2bb04f5ae2f7f
numdinkushi <numdinkushi@gmail.com>
2023-04-26 00:22:55 +0100

    added product migrations

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+   
     public function index()
     {
-        
+        $products = Product::orderBy('id', 'DESC')->get();
+        return view('backend.product.index', compact('products'));
     }
 
+    public function productStatus(Request $request)
+    {
+      if($request->mode == 'true'){
+        DB::table('products')->where('id', $request->id)->update(['status' => 'active']);
+    } else {
+        DB::table('products')->where('id', $request->id)->update(['status' => 'inactive']);
+    }
+    return response()->json(['msg' => 'successfully updated', 'status' => true]);
+    }
     /**
      * Show the form for creating a new resource.
      */
